6195192eb434288919dab4d97364161c43ac419f
Sarath-2803 <sarathkrishna357@gmail.com>
2026-02-07 20:16:07 +0530

    fixed render issue

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -7,6 +7,12 @@ const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
       dialect: 'postgres',
       logging: console.log,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
     })
   : new Sequelize({
       database: process.env.DB_NAME || 'canteen',
