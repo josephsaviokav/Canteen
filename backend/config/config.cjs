2c8e281e963fdbc2d0d59e91f940989fe212d2b1
Sarath-2803 <sarathkrishna357@gmail.com>
2026-02-07 20:35:57 +0530

    fixed render issue

--- backend/config/config.cjs
+++ backend/config/config.cjs
@@ -2,24 +2,31 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME || "postgres",
+    "username": process.env.DB_USER || "postgres",
     "password": process.env.DB_PASSWORD || "postgres",
     "database": process.env.DB_NAME || "canteen",
     "host": process.env.DB_HOST || "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.DB_USERNAME || "postgres",
+    "username": process.env.DB_USER || "postgres",
     "password": process.env.DB_PASSWORD || "postgres",
     "database": process.env.DB_NAME || "canteen",
     "host": process.env.DB_HOST || "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME || "postgres",
+    "use_env_variable": "DATABASE_URL",
+    "username": process.env.DB_USER || "postgres",
     "password": process.env.DB_PASSWORD || "postgres",
     "database": process.env.DB_NAME || "canteen",
     "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
